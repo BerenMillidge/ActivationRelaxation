6e75554d327369b146e4506d618eca36fb6ff415
Beren <bmillidgework@gmail.com>
2020-08-04 10:12:58 +0100

    update experiment files for multiple dataset

--- generate_backprop_experiments.py
+++ generate_backprop_experiments.py
@@ -7,10 +7,51 @@ exp_name = str(sys.argv[4])
 base_call = "python main.py"
 output_file = open(generated_name, "w")
 seeds = 5
-condition="backprop"
+
+condition="mnist_AR"
 for s in range(seeds):
     lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
     spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
-    final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --network_type bp"
+    final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) +" --dataset mnist"
+    print(final_call)
+    print(final_call, file=output_file)
+
+condition="mnist_BP"
+for s in range(seeds):
+    lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+    spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+    final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --dataset mnist" + " --network_type bp"
+    print(final_call)
+    print(final_call, file=output_file)
+
+condition="svhn_AR"
+for s in range(seeds):
+    lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+    spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+    final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) +" --dataset svhn"
+    print(final_call)
+    print(final_call, file=output_file)
+
+condition="svhn_BP"
+for s in range(seeds):
+    lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+    spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+    final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --dataset svhn" + " --network_type bp"
+    print(final_call)
+    print(final_call, file=output_file)
+
+condition="fashion_AR"
+for s in range(seeds):
+    lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+    spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+    final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) +" --dataset fashion"
+    print(final_call)
+    print(final_call, file=output_file)
+
+condition="fashion_BP"
+for s in range(seeds):
+    lpath = log_path + "/"+str(exp_name) +"_"+condition + "/" + str(s)
+    spath = save_path + "/" + str(exp_name) +"_"+condition + "/" + str(s)
+    final_call = base_call + " --logdir " + str(lpath) + " --savedir " + str(spath) + " --dataset fashion" + " --network_type bp"
     print(final_call)
     print(final_call, file=output_file)
